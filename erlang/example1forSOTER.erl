4d49a60d96007e6d07053aae040c994660482be6
Tormod Hellen <tormod.hellen@gmail.com>
2014-11-12 10:45:01 +0100

    added norsk

--- erlang/example1forSOTER.erl
+++ erlang/example1forSOTER.erl
@@ -5,18 +5,16 @@
 -uncoverable("noTrueLovelabel>0").
 
 main() ->
-  io:format("lol\n"),
-  Pidth1 = spawn(fun() -> the() end),
-  Pidth2 = spawn(fun() -> tho() end),
-  Pidth1 ! {Pidth2, cupidsArrow},
+  Pidthe = spawn(fun() -> the() end),
+  Pidtho = spawn(fun() -> tho() end),
+  Pidthe ! {Pidtho, cupidsArrow},
   sleep(2000),
   ok.
 
 the()->
 	receive
 	  {To, cupidsArrow} ->
-	    To ! {self(), unrequitedLove},
-	    io:format("lol2\n");
+	    To ! {self(), unrequitedLove};
 	  {From, trueLove} ->
       	?label("trueLovelabel"),
 	    io:format("Found true love!\n")
